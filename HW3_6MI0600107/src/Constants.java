8c1e27970f74d1c5661bd11ef53852ee9761dfba
Dian Vasilev <92683949+didov02@users.noreply.github.com>
2024-11-17 18:17:48 +0200

    Second solution

---
+++ HW3_6MI0600107/src/Constants.java
@@ -0,0 +1,9 @@
+public class Constants {
+    public final int POPULATION_SIZE = 100;
+    public final int MAX_GENERATIONS = 100;
+    public final double MUTATION_RATE = 0.05;
+    public final double CROSSOVER_RATE = 0.8;
+
+    public Constants() {
+    }
+}
